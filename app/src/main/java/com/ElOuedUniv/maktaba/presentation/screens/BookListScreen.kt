dc583224c6cae9291b52db4fdadc1451f7efab37
Mohsen <prooussama2022@gmail.com>
2026-02-18 21:28:21 +0100

    fix

--- app/src/main/java/com/ElOuedUniv/maktaba/presentation/screens/BookListScreen.kt
+++ app/src/main/java/com/ElOuedUniv/maktaba/presentation/screens/BookListScreen.kt
@@ -51,13 +51,14 @@ fun BookListScreen(
                         modifier = Modifier.align(Alignment.Center)
                     )
                 } else {
+                    Column{
                     Text(text = "Total Books: ${books.size}",
                         modifier = Modifier.padding(16.dp))
                     BookList(
                         books = books,
-                        
                         modifier = Modifier.fillMaxSize()
-                    )
+
+                    )}
                 }
             }
         }
